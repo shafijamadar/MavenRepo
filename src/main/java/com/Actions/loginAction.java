acf53180887595cfdeb13120439345fecc52c823
Mohammadshafi <Mohammadshafi@Lenovo-PC>
2017-04-05 21:46:30 +0530

    Action Class with constructor added

--- src/main/java/com/Actions/loginAction.java
+++ src/main/java/com/Actions/loginAction.java
@@ -1,5 +1,14 @@
 package com.Actions;
 
+import org.openqa.selenium.WebDriver;
+
 public class loginAction {
+	public WebDriver driver=null;
+	
+	public loginAction(WebDriver driver){
+		this.driver=driver;
+		
+		
+	}
 
 }
